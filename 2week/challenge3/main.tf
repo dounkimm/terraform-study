348b471fa1021e463099e029d192df95832856b5
dounkimm <98010656+dounkimm@users.noreply.github.com>
2023-07-15 23:47:12 +0900

    Create main.tf

---
+++ 2week/challenge3/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+# data 소스를 이용하여 기존에 생성되어 있던 subnet 사용
+data "aws_subnet" "doun-subnet" {
+  id = "<subnet id>"
+}
+
+# data 소스를 이용하여 기존에 생성되어 있던 security group 사용
+data "aws_security_group" "doun-sg" {
+  id = "<security group id>"
+}
+
+# ec2 name 입력 변수
+variable "name" {
+  type = string
+}
+
+resource "aws_instance" "test1" {
+  ami                    = "ami-0c9c942bd7bf113a2"
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnet.doun-subnet.id
+  vpc_security_group_ids = [data.aws_security_group.doun-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apache2
+              sudo echo "DoUn T101" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    # ec2 name을 입력변수를 활용하여 생성
+    Name = var.name
+  }
+}
