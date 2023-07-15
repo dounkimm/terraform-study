8784e1f96e5103ee0d5b16024d8a8e0723b1d1dc
dounkimm <98010656+dounkimm@users.noreply.github.com>
2023-07-15 23:44:52 +0900

    Create vpc.tf

---
+++ 2week/challenge2/vpc.tf
@@ -0,0 +1,72 @@
+provider "aws" {
+  region  = "ap-northeast-2"
+}
+
+resource "aws_vpc" "doun-vpc" {
+  cidr_block       = "10.10.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "doun-t101-study"
+  }
+}
+
+resource "aws_subnet" "doun-subnet1" {
+  vpc_id     = aws_vpc.doun-vpc.id
+  cidr_block = "10.10.1.0/24"
+
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "doun-t101-subnet1"
+  }
+}
+
+resource "aws_subnet" "doun-subnet2" {
+  vpc_id     = aws_vpc.doun-vpc.id
+  cidr_block = "10.10.2.0/24"
+
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "doun-t101-subnet2"
+  }
+}
+
+
+resource "aws_internet_gateway" "doun-igw" {
+  vpc_id = aws_vpc.doun-vpc.id
+
+  tags = {
+    Name = "doun-t101-igw"
+  }
+}
+
+resource "aws_route_table" "doun-rt" {
+  vpc_id = aws_vpc.doun-vpc.id
+
+  tags = {
+    Name = "doun-t101-rt"
+  }
+}
+
+resource "aws_route_table_association" "doun-rtassociation1" {
+  subnet_id      = aws_subnet.doun-subnet1.id
+  route_table_id = aws_route_table.doun-rt.id
+}
+
+resource "aws_route_table_association" "doun-rtassociation2" {
+  subnet_id      = aws_subnet.doun-subnet2.id
+  route_table_id = aws_route_table.doun-rt.id
+}
+
+resource "aws_route" "doun-defaultroute" {
+  route_table_id         = aws_route_table.doun-rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.doun-igw.id
+}
+
+output "aws_vpc_id" {
+  value = aws_vpc.doun-vpc.id
+}
